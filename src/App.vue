6353de50b7bcb403d7878e6b22d87df130849be4
Braedon <braedondavies@gmail.com>
2020-02-18 21:36:48 -0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h1>Todos</h1>
+    <ul>
+      <li v-for="todo of todos" :key="todo.id">
+        {{todo.name}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      todos: []
+    }
+  },
+  async created() {
+    try {
+      const res = await axios.get(`http://localhost:8000/todos`)
+
+      this.todos = res.data;
+    } catch(e) {
+      console.error(e)
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #6c5ce7;
+  margin-top: 60px;
+}
+
+li {
+  list-style: none;
+}
+
+h1 {
+  color: #a29bfe
+}
+</style>
