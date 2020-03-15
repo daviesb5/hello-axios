b5538cd5a41459a4284cd92c9686c031754227a0
Braedon <braedondavies@gmail.com>
2020-03-14 23:01:32 -0600

    committing changes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>Todos</h1>
+    <input type="text" v-model="todoName" @keyup.enter="addTodo">
     <ul>
       <li v-for="todo of todos" :key="todo.id">
         {{todo.name}}
@@ -12,39 +13,33 @@
 <script>
 import axios from 'axios';
 
+const baseURL = "http://localhost:8000/todos"
+
 export default {
   name: 'app',
   data() {
     return {
-      todos: []
+      todos: [],
+      todoName: ''
     }
   },
   async created() {
     try {
-      const res = await axios.get(`http://localhost:8000/todos`)
+      const res = await axios.get(baseURL)
 
       this.todos = res.data;
     } catch(e) {
       console.error(e)
     }
   },
+  methods: {
+    async addTodo() {
+      const res = await axios.post(baseURL, { name: this.todoName })
+
+      this.todos = [...this.todos, res.data]
+
+      this.todoName = ''
+    }
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #6c5ce7;
-  margin-top: 60px;
-}
-
-li {
-  list-style: none;
-}
-
-h1 {
-  color: #a29bfe
-}
-</style>
